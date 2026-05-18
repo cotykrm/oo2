c0bb11e415aba825e22f223a1d86a2501a8d9825
Constanza Krmpotic <coty.krm@gmail.com>
2026-05-18 15:20:29 -0300

    Ejercicio 17 modificación

--- PatronesDD/ejercicio17/src/main/java/ar/edu/unlp/info/oo1/ejercicio17/Connection4GAdapter.java
+++ PatronesDD/ejercicio17/src/main/java/ar/edu/unlp/info/oo1/ejercicio17/Connection4GAdapter.java
@@ -1,5 +1,15 @@
 package ar.edu.unlp.info.oo1.ejercicio17;
 
-public class Connection4GAdapter {
+public class Connection4GAdapter implements Connection{
+    
+    private Connection4G fourG;
+    
+    public String sendData(String data, int crc){
+        return this.fourG.transmit(data, crc);
+    }
+    
+    public String pict(){
+        return this.fourG.symb();
+    }
 
 }
